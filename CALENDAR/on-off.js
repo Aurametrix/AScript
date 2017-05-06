458c1a21c99165956609bba2800b2ae5f0d531f2
Aurametrix <irene@aurametrix.com>
2017-05-06 11:43:32 -0700

    response while turning a calendar on and off
    Google prepend while(1); to their (private) JSON responses to prevent JSON hijacking  http://haacked.com/archive/2009/06/25/json-hijacking.aspx/
    https://news.ycombinator.com/item?id=14280625

---
+++ CALENDAR/on-off.js
@@ -0,0 +1,4 @@
+while(1);[['u',[['smsSentFlag','false'],['hideInvitations','false'],
+  ['remindOnRespondedEventsOnly','true'],
+  ['hideInvitations_remindOnRespondedEventsOnly','false_true'],
+  ['Calendar ID stripped for privacy','false'],['smsVerifiedFlag','true']]]]
